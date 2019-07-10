8870e38a84d85fbc48c3f8bbed17533663414b7f
mantianxing <mantianxing@2dfire.com>
2019-07-10 16:59:32 +0800

    [update]从内部存储和外部存储中读取文件内容

--- app/src/main/java/com/example/sd/learningproject/storage/FileSaveActivity.java
+++ app/src/main/java/com/example/sd/learningproject/storage/FileSaveActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.example.sd.learningproject.R;
@@ -15,6 +18,11 @@ import java.io.*;
  * 将数据保存到文件中
  */
 public class FileSaveActivity extends AppCompatActivity {
+    @BindView(R.id.text_view_1)
+    TextView mTextViewFirst;
+    @BindView(R.id.text_view_2)
+    TextView mTextViewSecond;
+
     private String data = "Data to save";
 
     @Override
@@ -25,7 +33,7 @@ public class FileSaveActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.button1, R.id.button2})
+    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4})
     void click(View view) {
         switch (view.getId()) {
             case R.id.button1:
@@ -35,6 +43,20 @@ public class FileSaveActivity extends AppCompatActivity {
             case R.id.button2:
                 storage();
                 break;
+
+            case R.id.button3:
+                String string = load();
+                if (!TextUtils.isEmpty(string)) {
+                    mTextViewFirst.setText(string);
+                }
+                break;
+
+            case R.id.button4:
+                String content = loadExternal();
+                if (!TextUtils.isEmpty(content)) {
+                    mTextViewSecond.setText(content);
+                }
+                break;
         }
     }
 
@@ -42,7 +64,7 @@ public class FileSaveActivity extends AppCompatActivity {
         FileOutputStream outputStream = null;
         BufferedWriter writer = null;
         try {
-            outputStream = openFileOutput("wyqfile", Context.MODE_PRIVATE);
+            outputStream = openFileOutput("wyqfile111", Context.MODE_PRIVATE);
             writer = new BufferedWriter(new OutputStreamWriter(outputStream));
             writer.write(data);
         } catch (IOException e) {
@@ -77,5 +99,52 @@ public class FileSaveActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String load() {  // 从内部存储中读取数据
+        FileInputStream inputStream = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            inputStream = openFileInput("wyqfile111");
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.toString();
+    }
+
+    private String loadExternal() {  // 从外部存储中读取数据
+        File file = null;
+        BufferedReader reader = null;
+        FileInputStream fileInputStream = null;
+        StringBuilder content = new StringBuilder();
+
+        String dir = getExternalFilesDir("").getAbsolutePath() + "/";  // 外部存储下包名文件夹下的files文件中
+        try {
+            file = new File(dir + "wyqfile.txt");
+            fileInputStream = new FileInputStream(file);
+            reader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
     }
 }
