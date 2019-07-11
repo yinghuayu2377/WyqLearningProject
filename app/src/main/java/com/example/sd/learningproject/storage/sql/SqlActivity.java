6d63e7182b2b4c8dab175384dc0200c54965a6e4
mantianxing <mantianxing@2dfire.com>
2019-07-11 16:11:16 +0800

    [update]使用insert插入数据

--- app/src/main/java/com/example/sd/learningproject/storage/sql/SqlActivity.java
+++ app/src/main/java/com/example/sd/learningproject/storage/sql/SqlActivity.java
@@ -1,5 +1,7 @@
 package com.example.sd.learningproject.storage.sql;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,7 @@ import com.example.sd.learningproject.R;
  */
 public class SqlActivity extends AppCompatActivity {
     private MyDatabaseHelper databaseHelper = null;
+    private MyDatabaseHelper databaseHelperSecond = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,9 +24,10 @@ public class SqlActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
         databaseHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+        databaseHelperSecond = new MyDatabaseHelper(this, "BookStore.db", null ,2);
     }
 
-    @OnClick({R.id.button1, R.id.button2})
+    @OnClick({R.id.button1, R.id.button2, R.id.button3})
     void click(View view) {
         switch (view.getId()) {
             case R.id.button1:
@@ -31,8 +35,27 @@ public class SqlActivity extends AppCompatActivity {
                 break;
 
             case R.id.button2:
-                databaseHelper = new MyDatabaseHelper(this, "BookStore.db", null ,2);
-                databaseHelper.getWritableDatabase();
+                databaseHelperSecond.getWritableDatabase();
+                break;
+
+            case R.id.button3:
+                SQLiteDatabase sqLiteDatabase = databaseHelperSecond.getWritableDatabase();
+                ContentValues contentValues = new ContentValues();
+
+                // 插入第一条数据
+                contentValues.put("name", "The Da Vinci Code");
+                contentValues.put("author", "Dan Brown");
+                contentValues.put("pages", 23);
+                contentValues.put("price", 12.67);
+                sqLiteDatabase.insert("Book", null, contentValues);
+
+                // 插入第二条数据
+                contentValues.clear();
+                contentValues.put("name", "The Da Vinci Code 2");
+                contentValues.put("author", "Dan Brown 2");
+                contentValues.put("pages", 232);
+                contentValues.put("price", 12.672);
+                sqLiteDatabase.insert("Book", null, contentValues);
                 break;
         }
     }
