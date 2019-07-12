ff78dfcaeb8b6c1d9808dc0b3176e0a1f2d961fc
mantianxing <mantianxing@2dfire.com>
2019-07-12 14:27:19 +0800

    [update]使用LitePal创建数据库

---
+++ app/src/main/java/com/example/sd/learningproject/storage/litepal/Book.java
@@ -0,0 +1,62 @@
+package com.example.sd.learningproject.storage.litepal;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.crud.LitePalSupport;
+
+public class Book extends LitePalSupport {
+    private int id;
+    private String name;
+    private String author;
+    private int pages;
+    private float price;
+    private String press;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getPress() {
+        return press;
+    }
+
+    public void setPress(String press) {
+        this.press = press;
+    }
+
+}
