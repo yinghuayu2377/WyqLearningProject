0ff40881b17e760e9c59797a55ae3ab54bf5ea2b
mantianxing <mantianxing@2dfire.com>
2019-07-12 15:10:51 +0800

    [update]查询数据

--- app/src/main/java/com/example/sd/learningproject/storage/litepal/LitePalActivity.java
+++ app/src/main/java/com/example/sd/learningproject/storage/litepal/LitePalActivity.java
@@ -5,11 +5,15 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.example.sd.learningproject.R;
+import org.litepal.LitePal;
 import org.litepal.tablemanager.Connector;
+
+import java.util.List;
 
 /**
  * 使用LitePal存储数据
@@ -24,6 +28,8 @@ public class LitePalActivity extends AppCompatActivity {
     Button mButton9;
     @BindView(R.id.button10)
     Button mButton10;
+    @BindView(R.id.text_view)
+    TextView mTextView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,7 +43,7 @@ public class LitePalActivity extends AppCompatActivity {
         mButton10.setVisibility(View.GONE);
     }
 
-    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5})
+    @OnClick({R.id.button1, R.id.button2, R.id.button3, R.id.button4, R.id.button5, R.id.button6})
     void click(View view) {
         switch (view.getId()) {
             case R.id.button1:  // 创建数据库
@@ -63,6 +69,23 @@ public class LitePalActivity extends AppCompatActivity {
                 book1.setPress("Anchor");
                 book1.setPrice((float) 12.555);
                 book1.updateAll("name = ? and author = ?", "The Love of World", "mantianxing");
+                break;
+
+            case R.id.button5:  // 删除数据
+                LitePal.deleteAll(Book.class, "price > ?", "12");
+                break;
+
+            case R.id.button6:  // 查询数据
+                List<Book> list = LitePal.findAll(Book.class);
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Book book2 : list) {
+                    stringBuilder.append(book2.getName());
+                    stringBuilder.append(book2.getAuthor());
+                    stringBuilder.append(book2.getPress());
+                    stringBuilder.append(book2.getPrice());
+                    stringBuilder.append(book2.getPages());
+                }
+                mTextView.setText(stringBuilder);
                 break;
         }
     }
