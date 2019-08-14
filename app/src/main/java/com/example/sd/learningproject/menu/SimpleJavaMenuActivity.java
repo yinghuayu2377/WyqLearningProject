5763e956d585197f8004e574f1c1aa991dcc99a7
mantianxing <mantianxing@2dfire.com>
2019-08-14 10:57:04 +0800

    [update]使用java代码构建普通选项菜单

--- app/src/main/java/com/example/sd/learningproject/menu/SimpleJavaMenuActivity.java
+++ app/src/main/java/com/example/sd/learningproject/menu/SimpleJavaMenuActivity.java
@@ -3,6 +3,10 @@ package com.example.sd.learningproject.menu;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.example.sd.learningproject.R;
 
 /**
  * 用java代码构建的普通选项菜单
@@ -11,5 +15,28 @@ public class SimpleJavaMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem item1 = menu.add(0, 1, 1, "设置");
+        item1.setIcon(R.drawable.icon_blue_setting);
+        item1.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        MenuItem item2 = menu.add(0, 2, 2, "删除");
+        item2.setIcon(R.drawable.icon_delete);
+        item2.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == 1) {
+            Toast.makeText(this, "You clicked Setting", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (item.getItemId() == 2) {
+            Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
