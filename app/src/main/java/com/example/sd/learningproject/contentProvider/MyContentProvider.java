7e20591703c5508c48c9223fc06e92473a230659
mantianxing <mantianxing@2dfire.com>
2019-08-15 16:47:59 +0800

    [update]增加内容提供器

--- app/src/main/java/com/example/sd/learningproject/contentProvider/MyContentProvider.java
+++ app/src/main/java/com/example/sd/learningproject/contentProvider/MyContentProvider.java
@@ -1,4 +1,80 @@
-//package com.example.sd.learningproject.contentResolver;
+package com.example.sd.learningproject.contentProvider;
 
-public class MyContentProvider {
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * 自定义内容提供器
+ */
+public class MyContentProvider extends ContentProvider {
+    public static final int TABLE1_DIR = 0;
+    public static final int TABLE1_ITEM = 1;
+    public static final int TABLE2_DIR = 2;
+    public static final int TABLE2_ITEM = 3;
+    private static UriMatcher mUriMatcher;
+
+    static {
+        mUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        mUriMatcher.addURI("com.example.sd.learningproject.provider", "table1", TABLE1_DIR);
+        mUriMatcher.addURI("com.example.sd.learningproject.provider ", "table1/#", TABLE1_ITEM);
+        mUriMatcher.addURI("com.example.sd.learningproject.provider ", "table2", TABLE2_DIR);
+        mUriMatcher.addURI("com.example.sd.learningproject.provider ", "table2/#", TABLE2_ITEM);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        switch (mUriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                // 查询table1表中所有的数据
+                break;
+            case TABLE1_ITEM:
+                // 查询table1表中的单条数据
+                break;
+            case TABLE2_DIR:
+                // 查询table2表中所有的数据
+                break;
+            case TABLE2_ITEM:
+                // 查询table2表中的单条数据
+                break;
+        }
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (mUriMatcher.match(uri)) {
+            case TABLE1_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.sd.learningproject.provider.table1";
+            case TABLE1_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.sd.learningproject.provider.table1";
+            case TABLE2_DIR:
+                return "vnd.android.cursor.dir/vnd.com.example.sd.learningproject.provider.table2";
+            case TABLE2_ITEM:
+                return "vnd.android.cursor.item/vnd.com.example.sd.learningproject.provider.table2";
+        }
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
 }
