7bafd75080953886010a056051542ca0ff2d1b47
mantianxing <mantianxing@2dfire.com>
2019-09-03 14:45:27 +0800

    [update]增加SnackBar的使用

--- app/src/main/java/com/example/sd/learningproject/materialdesign/button/SuspendButtonActivity.java
+++ app/src/main/java/com/example/sd/learningproject/materialdesign/button/SuspendButtonActivity.java
@@ -3,6 +3,7 @@ package com.example.sd.learningproject.materialdesign.button;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -27,7 +28,14 @@ public class SuspendButtonActivity extends AppCompatActivity {
         mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(SuspendButtonActivity.this, "FAB clicked", Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, "Data deleted", Snackbar.LENGTH_SHORT)  // 使用Snackbar
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(SuspendButtonActivity.this, "FAB clicked", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
             }
         });
     }
