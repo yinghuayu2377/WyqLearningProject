f1312eb007030336760462e58196f6c71e145b6b
mantianxing <mantianxing@2dfire.com>
2019-09-03 14:11:04 +0800

    [update]用ToolBar代替ActionBar

--- app/src/main/java/com/example/sd/learningproject/materialdesign/toolbar/ToolBarActivity.java
+++ app/src/main/java/com/example/sd/learningproject/materialdesign/toolbar/ToolBarActivity.java
@@ -3,11 +3,47 @@ package com.example.sd.learningproject.materialdesign.toolbar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.example.sd.learningproject.R;
 
 public class ToolBarActivity extends AppCompatActivity {
+
+    @BindView(R.id.tool_bar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.context_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_add:
+                Toast.makeText(this, "You clicked Setting", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.item_remove:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
